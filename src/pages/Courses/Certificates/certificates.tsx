2256dcfa68fafc666291cfe794f597e071b86337
Gabriel <ghanel527@gmail.com>
2025-09-21 23:11:02 -0300

    feature: ajuste no carossel dos certificados

--- src/pages/Courses/Certificates/certificates.tsx
+++ src/pages/Courses/Certificates/certificates.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { CertificatesCarousel } from "../../../components/Carousel/Certificates/CertificatesCarousel";
+import styles from './certificates.module.css'
 
-const App: React.FC = () => {
+export default function Certificates() {
   return (
     <div style={{ width: "100%", height: "auto" }}>
       <div className="areaTitulo">
         <h1 className="titulo">CERTIFICADOS</h1>
       </div>
-      <CertificatesCarousel />
+      <div className={styles.areaCarouselCertificados}>
+              <CertificatesCarousel />
+      </div>
     </div>
   );
 };
-export default App;
